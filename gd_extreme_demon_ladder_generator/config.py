c5c330abf8cf2034172a28a3f67e5be6998d2431
Juan Montoya (Arby) <lic.jdavid23@gmail.com>
2026-09-02 17:40:27 -0500

    CONFIG: Added the AREDL API URL and Timeout time.

---
+++ gd_extreme_demon_ladder_generator/config.py
@@ -0,0 +1,10 @@
+# API ENDPOINTS
+
+
+# All Rated Extreme Demons List (AREDL)
+
+AREDL_BASE_URL = "https://api.aredl.net/v2/api"
+
+# CONFIGURACIONES TECNICAS
+
+REQUEST_TIMEOUT = 10  # Tiempo de espera para las solicitudes HTTP en segundos
