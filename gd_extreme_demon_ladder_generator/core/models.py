1fd57044519e2eaa5ac1f94f1c38e6af671e5122
Juan Montoya (Arby) <lic.jdavid23@gmail.com>
2026-09-03 15:16:25 -0500

    DELETE: Removed Publisher class and updated DemonLevel to use a string for publisher.

--- gd_extreme_demon_ladder_generator/core/models.py
+++ gd_extreme_demon_ladder_generator/core/models.py
@@ -3,41 +3,29 @@ from typing import Any
 
 
 @dataclass
-class Publisher:
-    id: str
-    global_name: str | None = field(default="Unknown")
-    
-    @classmethod
-    def from_json(cls, data: dict[str, Any]) -> "Publisher | None":
-        if not data:
-            return None
-        return cls(
-            id=data.get("id", ""),
-            global_name=data.get("global_name", "Unknown"),
-        )
-
-@dataclass
 class DemonLevel:
     id: str
     name: str
     position: int
     level_id: int
     gddl_tier: float | None = field(default=0)
-    tags: list[str]
-    song_id: int
-    publisher: Publisher | None
+    tags: list[str] | None = field(default_factory=list)
+    song_id: int | None = field(default=0)
+    publisher: str | None = field(default="")
     
     @classmethod
     def from_json(cls, data: dict[str, Any]) -> "DemonLevel | None":
         if not data:
             return None
+        
+        publisher = data.get("publisher",{})
         return cls(
             id=data.get("id", ""),
             name=data.get("name", "Unknown"),
-            position=data.get("position", ""),
+            position=data.get("position", 0),
             level_id=data.get("level_id", 0),
             gddl_tier=data.get("gddl_tier", 0),
             tags=data.get("tags", []),
             song_id=data.get("song_id", 0),
-            publisher=Publisher.from_json(data.get("publisher", {})),
+            publisher=publisher.get("global_name", ""),
         )
