6cc08c16738163f2d394567d83b9888b1724ac0e
Juan Montoya (Arby) <lic.jdavid23@gmail.com>
2026-09-03 15:14:51 -0500

    REFACTOR: Removed reduntant method bodies, merged into a helper method.

--- gd_extreme_demon_ladder_generator/api/aredl_client.py
+++ gd_extreme_demon_ladder_generator/api/aredl_client.py
@@ -5,6 +5,15 @@ class AREDLClient:
     def __init__(self, base_url: str, timeout: int):
         self.base_url = base_url
         self.timeout = timeout
+    def _get_request(self, endpoint: str):
+        response = requests.get(
+            endpoint,
+            timeout=self.timeout
+        )
+        
+        response.raise_for_status()
+        
+        return response
     def fetch_levels(self):
         """Fetches the list of all rated extreme demons from the AREDL API.
 
@@ -13,15 +22,20 @@ class AREDLClient:
         """
         endpoint = f"{self.base_url}/aredl/levels"
         
-        response = requests.get(
-            endpoint,
-            timeout=self.timeout
-        )
+        return self._get_request(endpoint).json()
+    def fetch_level(self, level_id: int) -> dict | None:
+        """Retrieves a level from the list based on its ID.
+
+        Args:    
+            level_id (int): The ID of the level to retrieve.
+
+        Returns:
+            dict | None: The level object if found, otherwise None.
+        """
         
-        response.raise_for_status()
-        
-        return response.json()
-    def get_level_by_id(self, levels: list[dict], level_id: int) -> dict | None:
+        endpoint = f"{self.base_url}/aredl/levels/{level_id}"
+        return self._get_request(endpoint).json()
+    def find_level_by_id(self, levels: list[dict], level_id: int) -> dict | None:
         """Retrieves a level from the list based on its ID.
 
         Args:
@@ -31,8 +45,8 @@ class AREDLClient:
         Returns:
             dict | None: The level object if found, otherwise None.
         """
-        return next((level for level in levels if level["level_id"] == level_id), None)
-    def get_level_by_position(self, levels: list[dict], position: int) -> dict | None:
+        return next((level for level in levels if level.get("level_id") == level_id), None)
+    def find_level_by_position(self, levels: list[dict], position: int) -> dict | None:
         """Retrieves a level from the list based on its position.
 
         Args:
@@ -43,7 +57,7 @@ class AREDLClient:
             dict | None: The level object if found, otherwise None.
         """
         return next((level for level in levels if level.get("position") == position), None)
-    def get_level_by_name(self, levels: list[dict], name: str) -> dict | None:
+    def find_level_by_name(self, levels: list[dict], name: str) -> dict | None:
         """Retrieves a level from the list based on its name.
 
         Args:
@@ -54,3 +68,14 @@ class AREDLClient:
             dict | None: The level object if found, otherwise None.
         """
         return next((level for level in levels if level["name"].lower() == name.lower()), None)
+    def find_publisher_by_id(self, levels: list[dict], publisher_id: str) -> dict | None:
+        """Retrieves a publisher from the list based on its ID.
+
+        Args:
+            levels (list[dict]): The list of levels.
+            publisher_id (str): The ID of the publisher to retrieve.
+
+        Returns:
+            dict | None: The publisher object if found, otherwise None.
+        """
+        return next((level for level in levels if level["publisher"]["id"] == publisher_id), None)
